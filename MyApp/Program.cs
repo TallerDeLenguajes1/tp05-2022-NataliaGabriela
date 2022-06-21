05db9b22b9f1f39d757f9c3d846ab5f393a14cd8
Natalia Lopez <natigab.24@gmail.com>
2022-06-20 22:17:01 -0500

    calculadora1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,22 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int NumeroIngresado, NumeroInvertido;
-NumeroInvertido= 0;
+int primerNumero, segundoNumero,opcionACalcular, flag=1;
+while(flag==1)
+{
+Console.WriteLine("Ingrese el primer numero para calcular");
+primerNumero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero para calcular");
+segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Ingrese el numero a invertir, debe ser mayor que cero");
-NumeroIngresado= Convert.ToInt32(Console.ReadLine());
-if (NumeroIngresado > 0)
+Console.WriteLine("Seleccione la operacion a calcular: 1.Suma, 2.Resta, 3.Multiplicaion, 4.Division");
+opcionACalcular = Convert.ToInt32(Console.ReadLine());
+
+switch (opcionACalcular)
 {
-while (NumeroIngresado > 0)
-{
-    NumeroInvertido= NumeroInvertido * 10 + NumeroIngresado % 10;
-    NumeroIngresado= NumeroIngresado/10;
+    case 1: Console.WriteLine("el resultado es: " + (primerNumero + segundoNumero));
+    break;
+    case 2: Console.WriteLine("el resultado es: " + (primerNumero - segundoNumero));
+    break;
+    case 3: Console.WriteLine("el resultado es: " + (primerNumero * segundoNumero));
+    break;
+    case 4:
+            if(segundoNumero!=0){
+                Console.WriteLine("el resultado es: " + (primerNumero / segundoNumero));
+            }else{
+                Console.WriteLine("el segundo debe ser distinto de cero");
+            }
+     
+    break;
+    default:
+            Console.WriteLine("debe escoger una opcion");
+    break;
 }
-  Console.WriteLine("El numero invertido es: " + NumeroInvertido);    
-}
-else
-{
-      Console.WriteLine("No se puede invertir porque es mayor que 0\n");
+Console.WriteLine("Desea realizar otra operacion?: 1.Si, 2.No");
+flag = Convert.ToInt32(Console.ReadLine());
 }
 
 
