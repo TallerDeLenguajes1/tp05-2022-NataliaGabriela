9fc6f830c2fa6bd402ca8e829db8bf7455c6c662
Natalia Lopez <natigab.24@gmail.com>
2022-06-21 00:38:58 -0500

    calculadora2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,22 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int primerNumero, segundoNumero;
+double numeroIngresado;
 
-int NumeroIngresado, NumeroInvertido;
-NumeroInvertido= 0;
+Console.WriteLine("Ingrese el numero para calcular");
+numeroIngresado = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Valor Absoluto:" + Math.Abs(numeroIngresado));
+Console.WriteLine("Cuadrado del numero:" + Math.Pow(numeroIngresado,2));
+if(numeroIngresado>0){
+        Console.WriteLine("Raiz Cuadrada:" + Math.Sqrt(numeroIngresado));
+    }else{
+        Console.WriteLine("Ingrese un numero mayor que cero");
+    }
 
-Console.WriteLine("Ingrese el numero a invertir, debe ser mayor que cero");
-NumeroIngresado= Convert.ToInt32(Console.ReadLine());
-if (NumeroIngresado > 0)
-{
-while (NumeroIngresado > 0)
-{
-    NumeroInvertido= NumeroInvertido * 10 + NumeroIngresado % 10;
-    NumeroIngresado= NumeroIngresado/10;
-}
-  Console.WriteLine("El numero invertido es: " + NumeroInvertido);    
-}
-else
-{
-      Console.WriteLine("No se puede invertir porque es mayor que 0\n");
-}
+Console.WriteLine("Seno:" + Math.Sin(numeroIngresado));
+Console.WriteLine("Coseno:" + Math.Cos(numeroIngresado));
+Console.WriteLine("Parte entera:" + Math.Truncate(numeroIngresado));
 
+Console.WriteLine("Ingrese el primer numero para calcular");
+primerNumero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero para calcular");
+segundoNumero = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("El mximo es : " + Math.Max(primerNumero,segundoNumero));
+Console.WriteLine("El minimmo es : " + Math.Min(primerNumero,segundoNumero));
